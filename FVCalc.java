1dbdfaa60f825f9250e200038b8b8c842f913134
Tal Gershtein <tal.gershtein@post.runi.ac.il>
2025-11-05 23:56:26 +0200

    Completed assignment

--- FVCalc.java
+++ FVCalc.java
@@ -2,9 +2,9 @@
 public class FVCalc {
 	public static void main(String[] args){
 		int currentValue = Integer.parseInt(args[0]);
-		double rate = Integer.parseInt(args[1]);
+		double rate = Double.parseDouble(args[1]);
 		int years = Integer.parseInt(args[2]);
 		int futureValue = (int)(currentValue * Math.pow((1 + rate/100), years));
-		System.out.println("After " + years +" years, " + currentValue + "$ saved at " + rate + "% will yield " + futureValue + "$.");
+		System.out.println("After " + years +" years, $" + currentValue + " saved at " + rate + "% will yield $" + futureValue + ".");
 	}
 }
